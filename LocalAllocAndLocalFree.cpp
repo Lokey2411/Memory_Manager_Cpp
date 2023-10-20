18c30126abbf93da23fbc38e2e6c6d591f21c039
Lokey2411 <hahaiviet24112003@gmail.com>
2023-10-21 01:56:42 +0700

    update du lieu

--- LocalAllocAndLocalFree.cpp
+++ LocalAllocAndLocalFree.cpp
@@ -1,24 +1,56 @@
 #include <windows.h>
 #include <iostream>
+#include <string>
+#define NUM_USER 10
 
-int main() {
+using namespace std;
+
+class User
+{
+public:
+    string displayName;
+    string username;
+    string password;
+    User() {}
+    User(string displayName, string username, string password)
+    {
+        this->displayName = displayName;
+        this->username = username;
+        this->password = password;
+    }
+    void display()
+    {
+        cout << "Name: " << this->displayName << ", username: " << this->username << ", pass: " << this->password << endl;
+    }
+    ~User()
+    {
+        cout << "Destructor called for " << displayName << endl;
+    }
+};
+
+int main()
+{
+    User data[] = {User("Viet beo", "viet2411", "123"), User("Viet xin", "viet2003", "456"), User("Gau truc", "viet24112003", "789"), User("Panda", "gautruc", "123"), User("Panda2", "gautruc2411", "456"), User("Gau truc 2", "gautruc24112003", "789"), User("23 con nai yeu em khong phai", "viet123", "0175"), User("Khong ngo gap phai thang lieu", "viet0175", "123"), User("Cuc chang da", "viet2415", "123"), User("Anh em minh cu phai goi la bung suc di", "mdh_0978****", "123")};
     // Cấp phát một khối bộ nhớ địa phương
-    HLOCAL hMem = LocalAlloc(LMEM_FIXED, 100 * sizeof(int));
-    if (hMem == NULL) {
-        std::cout << "Local memory allocation failed\n";
+    User *arr = (User *)LocalAlloc(LMEM_FIXED, NUM_USER * sizeof(User));
+    if (arr == NULL)
+    {
+        cout << "Local memory allocation failed\n";
         return 1;
     }
-
-    // Sử dụng bộ nhớ
-    int* arr = (int*)hMem;
-    for (int i = 0; i < 100; i++) {
-        arr[i] = i;
-        std::cout << arr[i] << " ";
+    for (int i = 0; i < NUM_USER; i++)
+        new (&arr[i]) User(data[i].displayName, data[i].username, data[i].password);
+    // cout << arr[0];
+    for (int i = 0; i < NUM_USER; i++)
+    {
+        cout << "User" << i << ": ";
+        arr[i].display();
     }
-    std::cout << std::endl;
+    cout << endl;
+    // delete[] arr;
 
     // Giải phóng bộ nhớ
-    LocalFree(hMem);
+    LocalFree(arr);
     system("pause");
 
     return 0;
--- VirtualAllocAndVirtualFree.cpp
+++ VirtualAllocAndVirtualFree.cpp
@@ -1,46 +1,58 @@
 #include <windows.h>
 #include <iostream>
+#include <string>
+#define NUM_USER 10
 
-class MemoryManager {
+using namespace std;
+
+class User
+{
 public:
-    // Hàm cấp phát bộ nhớ
-    LPVOID allocate(SIZE_T size) {
-        LPVOID lpvBase = VirtualAlloc(NULL, size, MEM_COMMIT, PAGE_READWRITE);
-        if (lpvBase == NULL) {
-            std::cout << "Memory allocation failed\n";
-            return NULL;
-        }
-        return lpvBase;
+    string displayName;
+    string username;
+    string password;
+    User() {}
+    User(string displayName, string username, string password)
+    {
+        this->displayName = displayName;
+        this->username = username;
+        this->password = password;
     }
-
-    // Hàm giải phóng bộ nhớ
-    BOOL deallocate(LPVOID lpvBase) {
-        if (VirtualFree(lpvBase, 0, MEM_RELEASE) == 0) {
-            std::cout << "Memory deallocation failed\n";
-            return FALSE;
-        }
-        return TRUE;
+    void display()
+    {
+        cout << "Name: " << this->displayName << ", username: " << this->username << ", pass: " << this->password << endl;
+    }
+    ~User()
+    {
+        cout << "Destructor called for " << displayName << endl;
     }
 };
 
-int main() {
-    MemoryManager mm;
-
-    // Cấp phát bộ nhớ cho mảng 10 phần tử
-    int* arr = (int*)mm.allocate(10 * sizeof(int));
-
-    // Kiểm tra nếu việc cấp phát bộ nhớ thành công
-    if (arr != NULL) {
-        // Sử dụng mảng
-        for (int i = 0; i < 10; i++) {
-            arr[i] = i;
-            std::cout << arr[i] << " ";
-        }
-        std::cout << std::endl;
-
-        // Giải phóng bộ nhớ
-        mm.deallocate(arr);
+int main()
+{
+    User data[] = {User("Viet beo", "viet2411", "123"), User("Viet xin", "viet2003", "456"), User("Gau truc", "viet24112003", "789"), User("Panda", "gautruc", "123"), User("Panda2", "gautruc2411", "456"), User("Gau truc 2", "gautruc24112003", "789"), User("23 con nai yeu em khong phai", "viet123", "0175"), User("Khong ngo gap phai thang lieu", "viet0175", "123"), User("Cuc chang da", "viet2415", "123"), User("Anh em minh cu phai goi la bung suc di", "mdh_0978****", "123")};
+    // Cấp phát một vùng bộ nhớ ảo
+    User *arr = (User *)VirtualAlloc(NULL, NUM_USER * sizeof(User), MEM_COMMIT, PAGE_READWRITE);
+    if (arr == NULL)
+    {
+        cout << "Virtual memory allocation failed\n";
+        return 1;
     }
+
+    for (int i = 0; i < NUM_USER; i++)
+        new (&arr[i]) User(data[i].displayName, data[i].username, data[i].password);
+    // cout << arr[0];
+    for (int i = 0; i < NUM_USER; i++)
+    {
+        cout << "User" << i << ": ";
+        arr[i].display();
+    }
+    cout << endl;
+    // delete[] arr;
+
+    // Giải phóng bộ nhớ
+    VirtualFree(arr, 0, MEM_RELEASE);
+    system("pause");
 
     return 0;
 }
