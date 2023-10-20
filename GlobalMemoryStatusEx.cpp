c11a6f7f51e347f80aa46499bb310be8e51d86d1
Lokey2411 <hahaiviet24112003@gmail.com>
2023-10-20 22:26:10 +0700

    update code&output

--- GlobalMemoryStatusEx.cpp
+++ GlobalMemoryStatusEx.cpp
@@ -3,18 +3,19 @@
 
 int main() {
     MEMORYSTATUSEX statex;
-
+    char bytesUnit[] = "GB";
+    const int bytesUnitChange = 1024*1024*1024;
     statex.dwLength = sizeof(statex);
 
     GlobalMemoryStatusEx(&statex);
 
-    std::cout << "There is  " << statex.dwMemoryLoad << " percent of memory in use.\n";
-    std::cout << "There are " << statex.ullTotalPhys/1024 << " total KB of physical memory.\n";
-    std::cout << "There are " << statex.ullAvailPhys/1024 << " free  KB of physical memory.\n";
-    std::cout << "There are " << statex.ullTotalPageFile/1024 << " total KB of paging file.\n";
-    std::cout << "There are " << statex.ullAvailPageFile/1024 << " free  KB of paging file.\n";
-    std::cout << "There are " << statex.ullTotalVirtual/1024  << " total KB of virtual memory.\n";
-    std::cout << "There are " << statex.ullAvailVirtual/1024  << " free  KB of virtual memory.\n";
+    std::cout << "Co " << statex.dwMemoryLoad << " % bo nho dang duoc su dung.\n";
+    std::cout << "Co tong cong " << statex.ullTotalPhys/bytesUnitChange << bytesUnit <<"   bo nho vat ly.\n";
+    std::cout << "Con lai " << statex.ullAvailPhys/bytesUnitChange << bytesUnit<<"   trong bo nho vat ly.\n";
+    std::cout << "Co tong cong " << statex.ullTotalPageFile/bytesUnitChange << bytesUnit<< "  trong tep trang.\n";
+    std::cout << "Con lai " << statex.ullAvailPageFile/bytesUnitChange << bytesUnit<<"  trong tep trang.\n";
+    std::cout << "Co tong cong " << statex.ullTotalVirtual/bytesUnitChange  <<bytesUnit<< "  trong bo nho ao.\n";
+    std::cout << "Con lai " << statex.ullAvailVirtual/bytesUnitChange  << bytesUnit<<" trong bo nho ao.\n";
 
     return 0;
 }
